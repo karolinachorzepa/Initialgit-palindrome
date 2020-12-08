d4bc81777afa510b77bc493085eed0d46cc54fba
Karolina Chorzepa <karolina.chorzepa@o2.pl>
2020-12-08 19:30:44 +0100

    Palindrome

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def isPalindromefunction(a):
+  return a==a [::-1]
+a="MADAM"
+ans=isPalindromefunction(a)
+if ans:
+  print("MADAM is a palindrome!!")
+else:
+  print("MADAM is not a palindrome")
+  
